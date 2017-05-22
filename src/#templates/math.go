80554b78b596bd81ea262fcda53cec6398b90fec
Eric Pan <erzpan@gmail.com>
2017-05-22 13:09:44 -0700

    Added GCD functions to math library

--- src/#templates/math.go
+++ src/#templates/math.go
@@ -26,3 +26,28 @@ func Abs(x int) int {
 		return x
 	}
 }
+
+func GCD(a, b int64) int64 {
+	if a < b {
+		return GCD(b, a)
+	}
+
+	r := a % b
+	if r == 0 {
+		return b
+	}
+
+	return GCD(b, r)
+}
+
+/*
+ * Takes the GCD of all input arguments.
+ * To use on arrays, change the argumen to be an array instead of variadic parameters.
+ */
+func GCDAll(args ...int64) int64 {
+	ans := args[0]
+	for i := 1; i < len(args); i++ {
+		ans = GCD(ans, args[i])
+	}
+	return ans
+}
